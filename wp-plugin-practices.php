bc8e1f6174d617eba71c553716c5dc3f62d86eb7
lvtute <lvtute@gmail.com>
2023-03-27 21:20:33 +0700

    done
    done add and remove custom post type

--- wp-plugin-practices.php
+++ wp-plugin-practices.php
@@ -32,22 +32,37 @@ defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
 
 class AlecadddPlugin
 {
+
+	function __construct()
+	{
+		add_action( 'init', array($this, 'custom_post_type'));
+	}
+
 	function activate() {
 		// generated a CPT
+		$this->custom_post_type();
+
 		// flush rewrite rules
 	}
 
 	function deactivate() {
 		// flush rewrite rules
+		flush_rewrite_rules();
 	}
 
 	function uninstall() {
 		// delete CPT
 		// delete all the plugin data from the DB
 	}
+
+	function custom_post_type() {
+		register_post_type( 'book', ['public' => 'true', 'label'=> 'Books']);
+	}
 }
 
- 
+if ( class_exists( 'AlecadddPlugin' ) ) {
+	$alecadddPlugin = new AlecadddPlugin();
+}
 
 // activation
 register_activation_hook( __FILE__, array( $alecadddPlugin, 'activate' ) );
